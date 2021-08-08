ccd371951564e1d863c41c0cf8db0b904e7535d6
suthirak <suthirakprom81@gmail.com>
2021-08-08 12:55:51 +0700

    petya and strings

---
+++ petyaAndStrings.py
@@ -0,0 +1,16 @@
+string1 = input().upper()
+string2 = input().upper()
+count = 0
+if string1 == string2:
+    print("0")
+else: 
+    for i in range(len(string1)):
+        if string1[i] < string2[i]:
+            count = -1  
+            break
+        elif string1[i] > string2[i]: 
+            count = 1
+            break
+        
+
+    print(count)
